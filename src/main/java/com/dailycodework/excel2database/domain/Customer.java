861a50883ba04732edad119f8b58323f378f5076
Utkarsh Doras <utkarshdoras0@gmail.com>
2023-03-17 16:03:34 +0530

    Added all the files

---
+++ src/main/java/com/dailycodework/excel2database/domain/Customer.java
@@ -0,0 +1,27 @@
+package com.dailycodework.excel2database.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private Integer customerId;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private Integer telephone;
+    private Date date;
+	
+
+}
